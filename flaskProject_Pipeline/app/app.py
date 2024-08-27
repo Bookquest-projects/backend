a8b418373ccc03d3a3e6490c79f64332733fcf39
SlWa99 <walid.slimani@hes-so.ch>
2024-08-27 09:55:45 +0200

    using of CV2 and add a class which allows to get book info by means of ISBN

--- flaskProject_Pipeline/app/app.py
+++ flaskProject_Pipeline/app/app.py
@@ -1,18 +1,27 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from app.BookRepository import BookRepository
 import cv2
 
 app = Flask(__name__)
-myInt = cv2.CV_16F
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!' + ' ' + str(myInt) + ' ' + str(cv2.BORDER_CONSTANT)
+    return "Hello, World! " + str(cv2.CV_64F)
 
 
-@app.route('/data')
-def data():
-    return jsonify({"key": "value"})
+@app.route('/book', methods=['GET'])
+def get_book_by_isbn():
+    isbn = request.args.get('isbn')
+    if not isbn:
+        return jsonify({"error": "ISBN is required"}), 400
+
+    bookRepository = BookRepository()
+    book_info = bookRepository.findBookByIsbn(isbn)
+    if not book_info:
+        return jsonify({"error": "Book not found"}), 404
+
+    return jsonify({"book_info": book_info})
 
 
 if __name__ == '__main__':

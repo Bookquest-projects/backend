06d209b29c94547ac5d1b758346fb22be65b78ab
SlWa99 <walid.slimani@hes-so.ch>
2024-08-30 14:19:59 +0200

    modifiy path in init.py

--- bookquest/app/__init__.py
+++ bookquest/app/__init__.py
@@ -1,8 +1,8 @@
 from flask import Flask
 from flask_cors import CORS
 
-from bookquest.app.auth import auth_bp
-from bookquest.app.books import books_bp
+from auth import auth_bp
+from books import books_bp
 
 app = Flask(__name__)
 CORS(app)

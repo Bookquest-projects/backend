18c1015067e5f0a37c4918f7ea8e0fcb3c4e66b7
SlWa99 <walid.slimani@hes-so.ch>
2024-09-01 18:15:44 +0200

    refactore: lint

--- bookquest/app/UserModel.py
+++ bookquest/app/UserModel.py
@@ -6,8 +6,7 @@ class User(db.Model):
     __tablename__ = 'user'
     id_user = Column(Integer, primary_key=True)
     username = Column(String(45), unique=True, nullable=False)
-    password = Column(String(200),
-                      nullable=False)
+    password = Column(String(200), nullable=False)
 
     def __repr__(self):
         return f'<User {self.username}>'

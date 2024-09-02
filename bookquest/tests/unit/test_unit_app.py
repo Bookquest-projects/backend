1852ab16743d0a9652a08cdd24ae427cb0a90391
peter <better_blood@hotmail.com>
2024-09-02 14:43:47 +0200

    test: unit test for valid isbn

--- bookquest/tests/unit/test_unit_app.py
+++ bookquest/tests/unit/test_unit_app.py
@@ -1,5 +1,7 @@
 import unittest
 
+from app.ocr import OCR
+
 
 def to_uppercase(s):
     """Retourne la chaîne en majuscules."""
@@ -13,6 +15,12 @@ class SimpleTestCase(unittest.TestCase):
         self.assertEqual(to_uppercase("world"), "WORLD")
         self.assertEqual(to_uppercase("Python"), "PYTHON")
         self.assertEqual(to_uppercase(""), "")
+
+    def test_is_valid_code(self):
+        """Teste la fonction is_valid_code."""
+        ocr = OCR()
+        self.assertEqual(ocr.is_valid_code("123456789X"), True)
+        self.assertEqual(ocr.is_valid_code("1234567890"), False)
 
 
 if __name__ == '__main__':

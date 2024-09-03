9b0998af41594d7e111f5f37257941f77771955f
SlWa99 <walid.slimani@hes-so.ch>
2024-09-03 19:44:22 +0200

    fix: update .env

--- bookquest/app/__init__.py
+++ bookquest/app/__init__.py
@@ -39,7 +39,7 @@ def create_app():
 # DATABASE CONFIGURATION #
 load_dotenv()
 DB_USER = os.getenv("DB_USER")
-DB_PASS = os.getenv("DB_PASS")
+DB_PASS = os.getenv("SQLSERVER_PASS")
 DB_NAME = os.getenv("DB_NAME")
 DB_HOST = os.getenv("DB_HOST")
 

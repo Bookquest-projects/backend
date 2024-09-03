588974a82b83a05570e771bdb19b62882a67acb6
Vicky <vicky.butty@heig-vd.ch>
2024-09-03 11:39:06 +0200

    feat : insert a test user

--- db/db_insert.sql
+++ db/db_insert.sql
@@ -8,3 +8,9 @@ VALUES
     ('To be read'),
     ('Not finished'),
     ('No way');
+
+--
+-- Insert a test user
+--
+INSERT INTO [user] ([username], [password])
+VALUES ('Admin', '$argon2id$v=19$m=65536,t=3,p=4$PaCybJopVsaeeospmU7aKQ$2w0pIvd+VPRWAEohwv/DWIZSy/KbsJLrD8Op9Bvl2q4');

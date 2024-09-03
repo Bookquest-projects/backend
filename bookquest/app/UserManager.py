e16b24ba186435a180b7839c141c46b0524f5abf
emilie-h <emilie.bressoud@heig-vd.ch>
2024-09-03 11:43:36 +0200

    fix: db

--- bookquest/app/UserManager.py
+++ bookquest/app/UserManager.py
@@ -1,46 +1,44 @@
-import os
 from argon2 import PasswordHasher
 from argon2.exceptions import VerifyMismatchError, VerificationError
 
 
 class UserManager:
 
-    def __init__(self, db_manager):
-        self.db_manager = db_manager
+    def __init__(self):
         self.ph = PasswordHasher()
 
-    def __hash_password(self, password, salt=None):
-        if not salt:
-            salt = os.urandom(16)
-        return self.ph.hash(password, salt.hex())
+    def __hash_password(self, password):
+        return self.ph.hash(password)
 
-    def __verify_password(self, hashed_password, password, salt):
+    def __verify_password(self, password, hashed_password):
         try:
-            return self.ph.verify(hashed_password, password + salt.hex())
+            return self.ph.verify(hashed_password, password)
         except (VerifyMismatchError, VerificationError):
             return False
 
     def __is_username_taken(self, username):
-        query = "SELECT COUNT(*) FROM users WHERE username = %s"
-        result = self.db_manager.execute_select(query, (username,))
-        return result[0][0] > 0
+        from bookquest.app import session, User
+        user = session.query(User).filter_by(username=username).first()
+        return user is not None
 
     def create_user(self, username, password):
         if self.__is_username_taken(username):
             raise ValueError("Username already used.")
 
-        salt = os.urandom(16)
-        hashed_password = self.__hash_password(password, salt)
-        query = ("INSERT INTO users (username, password, salt) "
-                 "VALUES (%s, %s, %s)")
-        self.db_manager.execute_insert(query, (
-            username, hashed_password, salt.hex()))
+        hashed_password = self.__hash_password(password)
+
+        from bookquest.app import User
+        user = User(username=username, password=hashed_password)
+
+        from bookquest.app import session
+        session.add(user)
+        session.commit()
+        return True
 
     def verify_user(self, username, password):
-        query = "SELECT password, salt FROM users WHERE username = %s"
-        result = self.db_manager.execute_select(query, (username,))
-        if result:
-            hashed_password, salt = result[0]
-            return self.__verify_password(hashed_password, password,
-                                          bytes.fromhex(salt))
-        return False
+        from bookquest.app import session, User
+        user = session.query(User).filter_by(username=username).first()
+        if user is None:
+            return False
+
+        return self.__verify_password(password, user.password)

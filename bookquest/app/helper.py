5ca01a7cdcfb577c5bb21dc83e8978dbc396c104
Vicky <vicky.butty@heig-vd.ch>
2024-09-03 09:48:29 +0200

    fix : add isbn structure check

--- bookquest/app/helper.py
+++ bookquest/app/helper.py
@@ -1,23 +1,29 @@
 def is_valid_isbn(isbn):
     def _validate_isbn10(isbn_10):
-        val = 0
+        total = 0
         for i in range(10, 1, -1):
-            val = val + i * int(isbn[10 - i])
-        return (11 - val % 11 == 10 and isbn[
-            -1] == "X") or 11 - val % 11 == int(isbn[-1])
+            total += i * int(isbn_10[10 - i])
+        check_digit = 11 - total % 11
+        return (check_digit == 10 and isbn_10[
+            -1] == "X") or check_digit == int(isbn_10[-1])
 
     def _validate_isbn13(isbn_13):
         if not isbn.startswith(("978", "979")):
             return False
 
-        val = 0
+        total = 0
         for i in range(12):
-            val += int(isbn[i]) * (1 if i % 2 == 0 else 3)
-        r = val % 10
-        return (r == 0 and r == int(isbn[12])) or 10 - r
+            total += int(isbn_13[i]) * (1 if i % 2 == 0 else 3)
+        check_digit = (10 - total % 10) % 10
+        return check_digit == int(isbn_13[-1])
 
     if len(isbn) == 10:
+        if not (isbn[:-1].isdigit() and (
+                isbn[-1].isdigit() or isbn[-1] == "X")):
+            return False
         return _validate_isbn10(isbn)
     elif len(isbn) == 13:
+        if not isbn.isdigit():
+            return False
         return _validate_isbn13(isbn)
     return False

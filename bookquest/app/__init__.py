d7b2f94761fcdf6191d1498c602074c585afeb7f
SlWa99 <walid.slimani@hes-so.ch>
2024-09-04 18:24:08 +0200

    ci: lint

--- bookquest/app/__init__.py
+++ bookquest/app/__init__.py
@@ -48,7 +48,8 @@ DB_HOST = os.getenv("DB_HOST")
 # reflect the tables
 Base = automap_base()
 engine = create_engine(
-    f"mssql+pytds://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}?instance=database-v001"
+    f"mssql+pytds://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}?instance"
+    f"=database-v001"
 )
 Base.prepare(engine)
 

eecf843893e7d7ff7f3771dd6c78e06a8c5e4b5b
Vicky <vicky.butty@heig-vd.ch>
2024-09-05 20:07:35 +0200

    fix : fix bookshelfs review and bookshelfs view

--- bookquest/app/bookshelf.py
+++ bookquest/app/bookshelf.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, jsonify
-from flask_jwt_extended import get_jwt_identity, verify_jwt_in_request
+from flask_jwt_extended import get_jwt_identity, jwt_required
 
 from BookRepository import BookRepository
 from UserManager import UserManager
@@ -11,40 +11,44 @@ bookshelf_bp = Blueprint('bookshelf', __name__)
 user_manager = UserManager()
 review_manager = ReviewManager()
 bookshelf_manager = BookshelfManager()
+book_repository = BookRepository()
 
 
 @bookshelf_bp.route('/bookshelf', methods=['GET'])
+@jwt_required()
 def bookshelf_name():
     try:
-        verify_jwt_in_request()
         current_username = get_jwt_identity()
 
         if 'name' not in request.args:
             return jsonify({"error": "Bookshelf name is required"}), 400
 
-        ALLOWED_NAME = bookshelf_manager.get_all_bookshelf_name()
-
         name = request.args.get('name')
+        if name in ['favorite', 'owned']:
+            user_id = user_manager.get_userid(current_username)
+            params = {
+                "fk_user": user_id,
+                name: True
+            }
+        else:
+            allowed_name = bookshelf_manager.is_bookshelf_name_allowed(name)
+            if not allowed_name:
+                return jsonify({"error": "Unsupported Bookshelf name"}), 415
 
-        if name not in ALLOWED_NAME:
-            print("Unsupported Bookshelf name: " + name)
-            return jsonify({"error": "Unsupported Bookshelf name"}), 415
+            user_id = user_manager.get_userid(current_username)
+            bookshelf_id = bookshelf_manager.get_bookshelfid_by_name(name)
+            params = {
+                "fk_user": user_id,
+                "fk_bookshelf": bookshelf_id
+            }
 
-        user_id = user_manager.get_userid(current_username)
-        bookshelf_id = bookshelf_manager.get_bookshelfid_by_name(name)
+        reviews = review_manager.get_review(params)
 
-        isbns_13 = review_manager.get_isbns13_by_userid_and_bookshelfid(
-            user_id, bookshelf_id)
-
-        book_repository = BookRepository()
         books = []
-
-        for isbn_13 in isbns_13:
-            books.append(book_repository.findBookByIsbn(isbn_13))
-
-        if len(books) == 0:
-            return jsonify({"error": "No books for this bookshelf"}), 200
+        for review in reviews:
+            books.append(book_repository.findBookByIsbn(review.isbn_13))
 
         return jsonify(books), 200
+
     except Exception as e:
         return jsonify({"error": str(e)}), 400
--- bookquest/app/bookshelfManager.py
+++ bookquest/app/bookshelfManager.py
@@ -2,11 +2,14 @@ from sqlalchemy import select
 
 
 class BookshelfManager:
+    def __init__(self):
+        self.allowed_bookshelves = self.get_all_bookshelf_name()
 
-    # def get_bookshelf_by_id(self, bookshelf_id):
-    #     query = "SELECT * FROM bookshelf WHERE id_bookshelf = %s"
-    #     result = self.db_manager.execute_select(query, (bookshelf_id,))
-    #     return result
+    def is_bookshelf_name_allowed(self, name: str):
+        print(self.allowed_bookshelves)
+        print(name)
+        print(f"Name is in list : {name in self.allowed_bookshelves}")
+        return name in self.allowed_bookshelves
 
     def get_bookshelfid_by_name(self, name: str):
         from __init__ import session, Bookshelf
@@ -21,8 +24,8 @@ class BookshelfManager:
     def get_all_bookshelf_name(self):
         from __init__ import session, Bookshelf
 
-        query = select(Bookshelf.name)
-        result = session.execute(query)
-        bookshelf_names = result.scalars().all()
-
+        # Get all bookshelf 's names
+        names = session.query(Bookshelf.name).all()
+        # Extract the names in a list
+        bookshelf_names = [name[0] for name in names]
         return bookshelf_names

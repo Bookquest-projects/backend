8ed0b2c1b876bbf517dc5079494ec6d1aa041f0c
emilie-h <emilie.bressoud@heig-vd.ch>
2024-09-05 18:31:18 +0200

    fix: errors

--- bookquest/app/bookshelf.py
+++ bookquest/app/bookshelf.py
@@ -8,7 +8,6 @@ from reviewManager import ReviewManager
 
 bookshelf_bp = Blueprint('bookshelf', __name__)
 
-
 user_manager = UserManager()
 review_manager = ReviewManager()
 bookshelf_manager = BookshelfManager()
@@ -16,34 +15,36 @@ bookshelf_manager = BookshelfManager()
 
 @bookshelf_bp.route('/bookshelf', methods=['GET'])
 def bookshelf_name():
-    verify_jwt_in_request()
-    current_username = get_jwt_identity()
+    try:
+        verify_jwt_in_request()
+        current_username = get_jwt_identity()
 
-    if 'name' not in request.args:
-        return jsonify({"error": "Bookshelf name is required"}), 400
+        if 'name' not in request.args:
+            return jsonify({"error": "Bookshelf name is required"}), 400
 
-    ALLOWED_NAME = bookshelf_manager.get_all_bookshelf_name()
-    print(ALLOWED_NAME)
-    name = request.args.get('name')
-    print(name)
+        ALLOWED_NAME = bookshelf_manager.get_all_bookshelf_name()
 
-    if name not in ALLOWED_NAME:
-        print("Unsupported Bookshelf name: " + name)
-        return jsonify({"error": "Unsupported Bookshelf name"}), 415
+        name = request.args.get('name')
 
-    user_id = user_manager.get_userid(current_username)
-    bookshelf_id = bookshelf_manager.get_bookshelfid_by_name(name)
+        if name not in ALLOWED_NAME:
+            print("Unsupported Bookshelf name: " + name)
+            return jsonify({"error": "Unsupported Bookshelf name"}), 415
 
-    isbns_13 = review_manager.get_isbns13_by_userid_and_bookshelfid(
-        user_id, bookshelf_id)
+        user_id = user_manager.get_userid(current_username)
+        bookshelf_id = bookshelf_manager.get_bookshelfid_by_name(name)
 
-    book_repository = BookRepository()
-    books = []
+        isbns_13 = review_manager.get_isbns13_by_userid_and_bookshelfid(
+            user_id, bookshelf_id)
 
-    for isbn_13 in isbns_13:
-        books.append(book_repository.findBookByIsbn(isbn_13))
+        book_repository = BookRepository()
+        books = []
 
-    if len(books) == 0:
-        return jsonify({"error": "No books for this bookshelf"}), 200
+        for isbn_13 in isbns_13:
+            books.append(book_repository.findBookByIsbn(isbn_13))
 
-    return jsonify(books), 200
+        if len(books) == 0:
+            return jsonify({"error": "No books for this bookshelf"}), 200
+
+        return jsonify(books), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
--- bookquest/app/review.py
+++ bookquest/app/review.py
@@ -16,136 +16,148 @@ bookshelf_manager = BookshelfManager()
 @review_bp.route('/reviews/<string:isbn>', methods=['GET'])
 @jwt_required()
 def get_review(isbn: str):
-    if not isbn:
-        return jsonify({"error": "ISBN is required"}), 400
+    try:
+        if not isbn:
+            return jsonify({"error": "ISBN is required"}), 400
 
-    if not is_valid_isbn(isbn):
-        return jsonify({"error": "ISBN is not valid"}), 400
+        if not is_valid_isbn(isbn):
+            return jsonify({"error": "ISBN is not valid"}), 400
 
-    current_username = get_jwt_identity()
-    user_id = user_manager.get_userid(current_username)
+        current_username = get_jwt_identity()
+        user_id = user_manager.get_userid(current_username)
 
-    isbn_key = "isbn_13" if len(isbn) == 13 else "isbn_10"
-    params = {
-        isbn_key: isbn,
-        "fk_user": user_id,
-    }
+        isbn_key = "isbn_13" if len(isbn) == 13 else "isbn_10"
+        params = {
+            isbn_key: isbn,
+            "fk_user": user_id,
+        }
 
-    review = review_manager.get_review(params)
-    if not review:
-        return jsonify({"error": "Review not found"}), 404
+        review = review_manager.get_review(params)
+        if not review:
+            return jsonify({"error": "Review not found"}), 404
 
-    review_infos = {
-        'isbn_13': review.isbn_13 or "",
-        'isbn_10': review.isbn_10 or "",
-        'ratings': review.ratings or "",
-        'favorite': review.favorite or False,
-        'owned': review.owned or False,
-        'reading_date': review.reading_date or "",
-        'reading_number': review.reading_number or "",
-        'comment': review.comment or ""
-    }
+        review_infos = {
+            'isbn_13': review.isbn_13 or "",
+            'isbn_10': review.isbn_10 or "",
+            'ratings': review.ratings or "",
+            'favorite': review.favorite or False,
+            'owned': review.owned or False,
+            'reading_date': review.reading_date or "",
+            'reading_number': review.reading_number or "",
+            'comment': review.comment or ""
+        }
 
-    return jsonify(review_infos), 200
+        return jsonify(review_infos), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
 
 
 # TODO : Refactor to have a function with duplicated code!
 @review_bp.route('/reviews/<string:isbn>/bookshelf', methods=['GET'])
 @jwt_required()
 def add_to_bookshelf(isbn: str):
-    if not isbn:
-        return jsonify({"error": "ISBN is required"}), 400
+    try:
+        if not isbn:
+            return jsonify({"error": "ISBN is required"}), 400
 
-    bookshelf_name = request.args.get('name', None)
-    if not bookshelf_name:
-        return jsonify({"error": "Bookshelf name is required"}), 400
+        bookshelf_name = request.args.get('name', None)
+        if not bookshelf_name:
+            return jsonify({"error": "Bookshelf name is required"}), 400
 
-    if not is_valid_isbn(isbn):
-        return jsonify({"error": "ISBN is not valid"}), 400
+        if not is_valid_isbn(isbn):
+            return jsonify({"error": "ISBN is not valid"}), 400
 
-    current_username = get_jwt_identity()
-    user_id = user_manager.get_userid(current_username)
-    bookshelf_id = bookshelf_manager.get_bookshelfid_by_name(
-        bookshelf_name)
+        current_username = get_jwt_identity()
+        user_id = user_manager.get_userid(current_username)
+        bookshelf_id = bookshelf_manager.get_bookshelfid_by_name(
+            bookshelf_name)
 
-    isbn_key = "isbn_13" if len(isbn) == 13 else "isbn_10"
-    params = {
-        isbn_key: isbn,
-        "fk_user": user_id,
-    }
+        isbn_key = "isbn_13" if len(isbn) == 13 else "isbn_10"
+        params = {
+            isbn_key: isbn,
+            "fk_user": user_id,
+        }
 
-    review = review_manager.get_review(params)
-    if not review:
-        params['bookshelf'] = bookshelf_id
-        success = review_manager.insert_review(params)
-    else:
-        updates = {'bookshelf': bookshelf_id}
-        success = review_manager.update_review(review, updates)
+        review = review_manager.get_review(params)
+        if not review:
+            params['bookshelf'] = bookshelf_id
+            success = review_manager.insert_review(params)
+        else:
+            updates = {'bookshelf': bookshelf_id}
+            success = review_manager.update_review(review, updates)
 
-    if not success:
-        return jsonify({"error": "Error while saving the review"}), 400
-    return jsonify(), 200
+        if not success:
+            return jsonify({"error": "Error while saving the review"}), 400
+        return jsonify(), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
 
 
 # TODO : Refactor to have a function with duplicated code!
 @review_bp.route('/reviews/<string:isbn>/favorites', methods=['POST'])
 @jwt_required()
 def add_to_favorite(isbn: str):
-    if not isbn:
-        return jsonify({"error": "ISBN is required"}), 400
+    try:
+        if not isbn:
+            return jsonify({"error": "ISBN is required"}), 400
 
-    if not is_valid_isbn(isbn):
-        return jsonify({"error": "ISBN is not valid"}), 400
+        if not is_valid_isbn(isbn):
+            return jsonify({"error": "ISBN is not valid"}), 400
 
-    current_username = get_jwt_identity()
-    user_id = user_manager.get_userid(current_username)
+        current_username = get_jwt_identity()
+        user_id = user_manager.get_userid(current_username)
 
-    isbn_key = "isbn_13" if len(isbn) == 13 else "isbn_10"
-    params = {
-        isbn_key: isbn,
-        "fk_user": user_id,
-    }
+        isbn_key = "isbn_13" if len(isbn) == 13 else "isbn_10"
+        params = {
+            isbn_key: isbn,
+            "fk_user": user_id,
+        }
 
-    review = review_manager.get_review(params)
-    if not review:
-        params['favorite'] = True
-        success = review_manager.insert_review(params)
-    else:
-        updates = {'favorite': (False if review.favorite else True), }
-        success = review_manager.update_review(review, updates)
+        review = review_manager.get_review(params)
+        if not review:
+            params['favorite'] = True
+            success = review_manager.insert_review(params)
+        else:
+            updates = {'favorite': (False if review.favorite else True), }
+            success = review_manager.update_review(review, updates)
 
-    if not success:
-        return jsonify({"error": "Error while saving the review"}), 400
-    return jsonify(), 200
+        if not success:
+            return jsonify({"error": "Error while saving the review"}), 400
+        return jsonify(), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
 
 
 # TODO : Refactor to have a function with duplicated code!
 @review_bp.route('/reviews/<string:isbn>/owned', methods=['POST'])
 @jwt_required()
 def add_to_owned(isbn: str):
-    if not isbn:
-        return jsonify({"error": "ISBN is required"}), 400
+    try:
+        if not isbn:
+            return jsonify({"error": "ISBN is required"}), 400
 
-    if not is_valid_isbn(isbn):
-        return jsonify({"error": "ISBN is not valid"}), 400
+        if not is_valid_isbn(isbn):
+            return jsonify({"error": "ISBN is not valid"}), 400
 
-    current_username = get_jwt_identity()
-    user_id = user_manager.get_userid(current_username)
+        current_username = get_jwt_identity()
+        user_id = user_manager.get_userid(current_username)
 
-    isbn_key = "isbn_13" if len(isbn) == 13 else "isbn_10"
-    params = {
-        isbn_key: isbn,
-        "fk_user": user_id,
-    }
+        isbn_key = "isbn_13" if len(isbn) == 13 else "isbn_10"
+        params = {
+            isbn_key: isbn,
+            "fk_user": user_id,
+        }
 
-    review = review_manager.get_review(params)
-    if not review:
-        params['owned'] = True
-        success = review_manager.insert_review(params)
-    else:
-        updates = {'owned': (False if review.owned else True), }
-        success = review_manager.update_review(review, updates)
+        review = review_manager.get_review(params)
+        if not review:
+            params['owned'] = True
+            success = review_manager.insert_review(params)
+        else:
+            updates = {'owned': (False if review.owned else True), }
+            success = review_manager.update_review(review, updates)
 
-    if not success:
-        return jsonify({"error": "Error while saving the review"}), 400
-    return jsonify(), 200
+        if not success:
+            return jsonify({"error": "Error while saving the review"}), 400
+        return jsonify(), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400

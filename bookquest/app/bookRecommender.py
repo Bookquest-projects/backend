02d8134bc892fe0ffb725fbdd9b9134291c67f3e
Vicky <56110187+Kalysso@users.noreply.github.com>
2024-09-05 08:42:47 +0200

    fix : fix uses of undefined function after helper creation (#41)
    * fix : fix uses of undefined function after helper creation
    
    * fix : fix linting

--- bookquest/app/bookRecommender.py
+++ bookquest/app/bookRecommender.py
@@ -64,11 +64,12 @@ class BookRecommender:
         # Fetch the target book
         book_repository = BookRepository()
         target_book = book_repository.findBookByIsbn(self.target_isbn)
-        target_category = target_book.get("categories")[0]
-        target_language = target_book.get("languages")
-
+        target_category = target_book.get("categories")
         if not target_category:
             return None
+
+        target_category = target_category[0]
+        target_language = target_book.get("languages")
 
         # Try to detect the lang if none is returned by the API
         if not target_language and target_book.get("description"):
--- bookquest/app/ocr.py
+++ bookquest/app/ocr.py
@@ -6,7 +6,6 @@ from pyzbar.pyzbar import decode
 
 regex_13 = r'9(?:[ -]*7)(?:[ -]*[89])(?:[ -]*\d){10}'
 regex_10 = r'\d(?:[ -]*\d){8}(?:[ -]*[\dX])'
-unwanted_chars_default = ['-', ' ']
 
 
 class OCR:
@@ -16,18 +15,6 @@ class OCR:
 
     def imread(self, path: str):
         return cv2.imread(path, cv2.IMREAD_GRAYSCALE)
-
-    def clean_isbn(self, isbn: str,
-                   chars_to_remove=None) -> str:
-        if chars_to_remove is None:
-            chars_to_remove = list()
-        if len(chars_to_remove) == 0:
-            chars_to_remove = unwanted_chars_default.copy()
-
-        isbn_cleaned = '%s' % isbn
-        for c in chars_to_remove:
-            isbn_cleaned = isbn_cleaned.replace(c, '')
-        return isbn_cleaned
 
     def get_bar_code_info(self, img_cv2):
         detectedBarcodes = decode(img_cv2)

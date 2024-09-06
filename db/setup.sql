30d05e8ba7375f997b9c40bbf15f8ff090195906
Walid <94690298+SlWa99@users.noreply.github.com>
2024-09-06 21:34:28 +0200

    Update setup.sql

--- db/setup.sql
+++ db/setup.sql
@@ -161,24 +161,3 @@ VALUES ('9781234567897', -- isbn_13
     1,                   -- fk_user
     1                    -- fk_bookshelf
     );
-INSERT INTO [review] ([isbn_13],
-    [isbn_10],
-    [ratings],
-    [favorite],
-    [owned],
-    [reading_date],
-    [reading_number],
-    [comment],
-    [fk_user],
-[fk_bookshelf])
-VALUES ('9780451488336', -- isbn_13
-    '123456789X',        -- isbn_10
-    5,                   -- ratings
-    1,                   -- favorite
-    1,                   -- owned
-    '2024-09-03',        -- reading_date
-    1,                   -- reading_number
-    'Great book!',       -- comment
-    1,                   -- fk_user
-    2                    -- fk_bookshelf
-    );
